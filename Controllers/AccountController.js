f419e47eac2d22cb7bd16c6f05fa0ceedf93526f
Ayeni Stanley <ayenistanley3@gmail.com>
2026-09-03 22:14:55 +0100

    add createAccount (from nibss)

---
+++ Controllers/AccountController.js
@@ -0,0 +1,40 @@
+const { User } = require("../Models/User");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { getAuthToken } = require('../AdaptToExternalAPI/ConnectToNibssByPhoenixAPI');
+
+module.exports = {
+    createAccount: async (kycType, kycId, dateOfBirth) => {
+        try {
+            //generate token
+            const token = await getAuthToken();
+
+            // Send http request to NIBSS simulator
+            const response = await axios.post(
+                `${process.env.NIBSS_BASE_URL}/api/account/create`,
+                {
+                    kycType: kycType, // 'bvn' or 'nin'
+                    kycID: kycId,
+                    dob: dateOfBirth, // 'YYYY-MM-DD'
+                },
+                {
+                    headers: {
+                        // 3. Fixed authorization template string syntax
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    }
+                }
+            );
+
+            return response;
+
+        } catch (error) {
+            return error;
+        }
+    }
+}
+
+
+
