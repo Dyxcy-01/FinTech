5d4d45d37a5429a2bf1ebf3acb73367755c7b479
Ayeni Stanley <ayenistanley3@gmail.com>
2026-09-04 00:34:47 +0100

    create route to create an account

--- Routes/AccountRoutes.js
+++ Routes/AccountRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { getAccounts, getBalance } = require('../Controllers/AccountController');
+const { getAccounts, getBalance, verifyReceipientName, createAccount } = require('../Controllers/AccountController');
 
+// CREATE
+// Create account
+router.post('/createAccount', createAccount);
 
+// READ
 // Admin access
 router.get('/accounts', getAccounts);
 
 // get user balance
 router.get('/account/balance/:accountNo', getBalance);
+
+// verify account name
+router.get('/account/verifyAcountName/:accountNo', verifyReceipientName);
