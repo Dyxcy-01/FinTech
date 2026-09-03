3c9d01a6e8a144e8867c35cd9ff8b67a7f00fc18
Ayeni Stanley <ayenistanley3@gmail.com>
2026-09-03 23:52:44 +0100

    create check transaction query method

--- Controllers/TransactionsController.js
+++ Controllers/TransactionsController.js
@@ -13,11 +13,11 @@ module.exports = {
             const response = await axios.get(
                 `${process.env.NIBSS_BASE_URL}/api/account/name-enquiry/${accountNumber}`,
                 {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json"
-                    }
-                }
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+                },
             );
 
             if (response && response.data) {
@@ -35,27 +35,70 @@ module.exports = {
             const response = await axios.post(
                 `${process.env.NIBSS_BASE_URL}/api/transfer`,
                 {
-                    from: senderAccountNo,
-                    to: recipientAccountNo,
-                    amount: amount,
+                from: senderAccountNo,
+                to: recipientAccountNo,
+                amount: amount,
                 },
                 {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        "Content-Type": "application/json",
-                    },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
                 },
             );
 
             // return response
             // if (response && response.data) {
-                return response.data;
+            return response.data;
             // }
-
         } catch (error) {
             return error;
         }
-    }
-}
+    },
+
+    // query transaction status(interbank transfer)
+    checkTransactionStatus: async (transactionId) => {
+        const token = await getAuthToken();
+
+        try {
+            const response = await axios.get(
+                `${process.env.NIBSS_BASE_URL}/api/transaction/${transactionId}`,
+                {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+                },
+            );
+
+            // return response
+            return response.data;
+        } catch (error) {
+            return error;
+        }
+    },
+
+    // query transaction status(interbank transfer)
+    checkTransactionStatus: async (transactionId) => {
+        const token = await getAuthToken();
+
+        try {
+            const response = await axios.get(
+                `${process.env.NIBSS_BASE_URL}/api/transaction/${transactionId}`,
+                {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                },
+                },
+            );
+
+            // return response
+            return response.data;
+        } catch (error) {
+            return error;
+        }
+    },
+};
 
 // {     "accountNumber": "1084071287",     "accountName": "John Doe",     "bankName": "Phc bank" } 
