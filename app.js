16a827e4b9150c772ff93220b1694f603e2cc2ee
Ayeni Stanley <ayenistanley3@gmail.com>
2026-09-09 19:12:44 +0100

    Install and use CORS package to prevent the error.

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const accountRoutes = require('./Routes/AccountRoutes');
 const transactionRoutes = require('./Routes/TransactionRoutes');
 const userRoutes = require('./Routes/UserRoutes');
 const utilitiesRoutes = require('./Routes/UtilitiesRoutes');
+const cors = require("cors");
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use('/accounts', accountRoutes);
 app.use('/transactions', transactionRoutes);
 app.use('/users', userRoutes);
 app.use('/utilities', utilitiesRoutes);
+
 
 module.exports = app;
